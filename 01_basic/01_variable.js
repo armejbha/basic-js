a785801aa9b2fe4c9baf65232733effe3e77c2bc
Abdur Rahim <96991837+armejbha@users.noreply.github.com>
2024-12-10 17:00:07 +0000

    datatypes and variable

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,10 @@
+const accountId=12234;
+let acocountEmail="mejbha@gmail.com";
+var accountPassword="1234";
+accountCity="jaipur"; // this is working just because of js advance machines. 
+//i can declare any variable not using const ,let, var.
+let accountState;
+// this result show undefined for value doesn't assinging here.
+//  accountId=241234; if i redeclare any const variable this is not working. 
+console.log(accountId);
+console.table([accountId, accountPassword, accountCity,accountState]);
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,18 @@
+let name="mejbha";
+let age=25;
+let isMejhbhaMarried= false;
+
+
+/*
+there are several type of data types
+1. number 2 to power 53
+2. bitInt
+3. string => ""
+4. boolean => true/false
+5. null => standlone value  (object is datatypes)
+6. undefiend
+7. symbol => unique
+*/
+
+console.log(typeof null);
+console.log(typeof undefined);
