b87026c3222b45eee53ca21c574e95261bdea6ce
Abdur Rahim <96991837+armejbha@users.noreply.github.com>
2024-12-11 16:29:30 +0000

    comparism

---
+++ 01_basic/04_comparism.js
@@ -0,0 +1,21 @@
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2>=1)
+// console.log(2==1)
+// console.log(2!=1)
+
+// this comparism is easy it's doesn't have any problem.
+
+// console.log("02" > 1);
+
+// console.log(null >0)
+// console.log(null ==0)
+// console.log(null >=0) this line null convert into number 
+
+// here create a problem in javascript equality and comparism doesn't work similary 
+
+// console.log(undefined >1)
+// console.log(undefined ==1)
+// console.log(undefined >=1)
+
+// == and === have different == just check value and === also check datatypes
