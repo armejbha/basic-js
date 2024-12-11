ba9735a518206dd125c72e0cd5f07acb2ed77cc9
Abdur Rahim <96991837+armejbha@users.noreply.github.com>
2024-12-11 06:12:06 +0000

    conversion

---
+++ 01_basic/03_conversionOperations.js
@@ -0,0 +1,56 @@
+const age="mejbha";
+
+const convertedToNUmber= Number(age)
+
+//  console.log(typeof convertedToNUmber);
+//  console.log(typeof (convertedToNUmber));
+
+//  console.log(convertedToNUmber);
+
+ /*
+        Number Conversion
+// 25=> 25
+// "25" => 25
+// ""=> 0
+// "25abc"=> NaN
+// "mejbha"=> NaN
+// null=> 0
+// undefined => NaN
+// true => 1; false =>0
+
+ */
+
+let myName= null;
+// console.log(typeof myName)
+
+const conversionString= String(myName)
+
+// console.log(typeof conversionString);
+// console.log(typeof (conversionString));
+// console.log(conversionString);
+
+/*
+    String conversion
+//  33=> "33"
+// "mejbha"=> "mejbha"
+// "undefined"=> undefined
+// null => null
+// true => true; false=> false
+
+
+*/
+
+const isSheCome =undefined;
+
+
+const conversionBoolean= Boolean(isSheCome);
+
+console.log(typeof conversionBoolean);
+console.log(conversionBoolean)
+
+/*
+    conversiton Boolean
+// 1=> true ; 0=> false
+// "mejbha"=> true; ""=> false
+// null=> false ; undefined=> false
+*/
