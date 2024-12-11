486e98a4f6f3d77aef47a880c53977ddd6bbaa79
Abdur Rahim <96991837+armejbha@users.noreply.github.com>
2024-12-11 17:09:32 +0000

    summary

---
+++ datatypesSummary.js
@@ -0,0 +1,31 @@
+// call by value and call by reference
+
+// primitive value
+
+let age=25;
+let name="mejbha";
+let isJobConfirm= false;
+let temperature=null;
+let salary;
+
+let id=Symbol('123');
+let anotherId=Symbol('123');
+
+// console.log(id===anotherId)
+
+// non-primitive 
+
+// Array, object, function 
+
+const before=["dk", "ridoy","jusi apu"," kaisen"];
+
+{
+    name:"jusi";
+    size:36;
+    isSheMarried: true;
+}
+
+const myFunction =function(){
+console.log(36)
+}
+myFunction();
