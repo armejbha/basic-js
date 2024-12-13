0daaa311a890aefba1ea8f20b39224e5fe1e4fba
Abdur Rahim <96991837+armejbha@users.noreply.github.com>
2024-12-13 16:15:23 +0000

    string

--- 01_basic/05_string.js
+++ 01_basic/05_string.js
@@ -7,5 +7,16 @@ const age=25;
 
 const newName= new String("mejbha");
 
-console.log(newName);
+// console.log(newName.toUpperCase());
+// console.log(newName.charAt());
+// console.log(newName.includes("fun"))
+// console.log(newName.indexOf("e"))
+// const anotherName=newName.slice(-5,3);
+// console.log(anotherName)
+// console.log(newName.split(' '))
+// console.log(newName.substring(0,6));
+
+const url="https:eeeeeeeeee";
+
+console.log(url.replace("e","t"))
 
