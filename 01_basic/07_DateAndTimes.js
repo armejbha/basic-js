62a3416ec689f5d16dc2224afc0e92932d64cc52
Abdur Rahim <96991837+armejbha@users.noreply.github.com>
2024-12-14 06:55:52 +0000

    unfinished

---
+++ 01_basic/07_DateAndTimes.js
@@ -0,0 +1,14 @@
+// Dates 
+
+let myDate= new Date();
+// console.log(myDate)
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+let myCreateDate= new Date(2024,11,14,5,33);
+console.log(myCreateDate.getDay())
+console.log(myCreateDate.toDateString());
+
+let myTimeStamp= Date.now();
+
+console.log(myTimeStamp.getTime)
