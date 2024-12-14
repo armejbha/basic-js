4151b1aa60b0cb5f887163910e89f21501a5e8b2
Abdur Rahim <96991837+armejbha@users.noreply.github.com>
2024-12-14 05:58:11 +0000

    math

--- 01_basic/06_numberAndMats.js
+++ 01_basic/06_numberAndMats.js
@@ -9,4 +9,21 @@ const balance= new Number(123.662)
 
 const account=new Number(10000000);
 
-console.log(account.toLocaleString("en-in"))
+// console.log(account.toLocaleString("en-in"))
+
+//++++++++++++++++++++++Maths+++++++++++++++++++++++++
+
+// console.log(Math.round(5.5))
+// console.log(Math.ceil(2.3))
+// console.log(Math.floor(2.4))
+// console.log(Math.max(2,3,4,6))
+// console.log(Math.min(2,3,4,6))
+// math.random range [0-1]
+// console.log((Math.random()*10)+1)
+
+const max=20;
+const min=10;
+
+// console.log(Math.floor(Math.random()*(max-min+1)))
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
