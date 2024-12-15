0bfc09fd831a59f149d3c996b051e268d3334102
Abdur Rahim <96991837+armejbha@users.noreply.github.com>
2024-12-15 15:43:26 +0000

    arr

---
+++ 02_basic/01_array.js
@@ -0,0 +1,30 @@
+const arr=[0,3,5,7,4,9];
+const myHeros=["Khalid","ali","tarek"];
+
+const newArr= new Array(1,2,3,4,5);
+
+// console.log(arr);
+// console.log(myHeros);
+// console.log(newArr);
+
+// console.log(arr[1])
+// console.log(arr.indexOf(5))
+// console.log(arr.includes(3))
+// console.log(myHeros.includes("Khalid"))
+
+// console.log(arr.push(11))
+// console.log(arr.pop())
+// console.log(arr)
+
+// arr.unshift(11)  add value in first element
+// console.log(arr)
+// arr.shift()
+// console.log(arr) removes from first element
+console.log("A:" ,arr);
+// const arr1=arr.slice(1,3); doesn't effect main array
+console.log("B:",arr1);
+
+// const arr2=arr.splice(1,3); it's totally changed and cutoff the value
+console.log("C:",arr2);
+console.log("D:",arr);
+
