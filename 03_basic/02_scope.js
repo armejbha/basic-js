462068f695b628c92aab7311094a89801f6c46ae
Abdur Rahim <96991837+armejbha@users.noreply.github.com>
2024-12-27 16:58:48 +0000

    scope

---
+++ 03_basic/02_scope.js
@@ -0,0 +1,7 @@
+// { } within this anything is under block scope .
+
+// above anything is global scope 
+
+// from block scope global scope value can be controll but global scope cann't access block scope value
+
+
