ba8c96c253d63f2b363a8ae8e162d1929cf3f5e4
Abdur Rahim <96991837+armejbha@users.noreply.github.com>
2024-12-29 17:05:05 +0000

    control

---
+++ 04_control/one.js
@@ -0,0 +1,18 @@
+if(true){
+    console.log("this is flow");
+}
+else{
+    console.log("this is not flow");
+}
+
+
+
+const isLogedIn=true;
+const isPassOk= true;
+
+if(isLogedIn && isPassOk){
+    console.log("lets his to login ");
+}
+else{
+    console.log("Tell his give write username and pass")
+}
---
+++ 04_control/switch.js
@@ -0,0 +1,19 @@
+const frinds="mejbha";
+
+switch (frinds) {
+    case "rakib":
+        console.log("he is yours friends") 
+        break;
+    case "mofij":
+        console.log("he is yours friends") 
+        break;
+    case "kafij":
+        console.log("he is yours fri") 
+        break;
+    case "mejbha":
+        console.log("he is yours f") 
+        break;
+
+    default:
+        break;
+}
