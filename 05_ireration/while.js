6ced68df952f8825cbf74c1a8f1bbd588c282984
Abdur Rahim <96991837+armejbha@users.noreply.github.com>
2024-12-30 07:21:03 +0000

    while

---
+++ 05_ireration/while.js
@@ -0,0 +1,18 @@
+let index =1;
+while (index<=10) {
+    // console.log(`the number is ${index}`)
+    index++;
+}
+
+let books=["english","Bangla","physics"];
+let i=0;
+while (i<books.length) {
+    // console.log(books[i]);
+    i++
+}
+let j=0;
+
+do {
+    console.log(j);
+    j++
+} while (j<=10);
