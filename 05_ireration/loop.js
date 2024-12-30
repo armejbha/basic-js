7624cad3d5741ca8ce0d294cfe72c940ad593707
Abdur Rahim <96991837+armejbha@users.noreply.github.com>
2024-12-30 03:22:28 +0000

    loop

---
+++ 05_ireration/loop.js
@@ -0,0 +1,39 @@
+for (let index = 0; index <=10; index++) {
+    // console.log(`The number is: ${index}`);
+    
+}
+
+for (let i = 0; i <=12; i++) {
+    if (i==5) {
+        // console.log(`${i} is the best number`)
+        
+    }
+    
+}
+const myArray=["mejbha","rahim","shakil"]
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element)
+    
+}
+
+
+for (let i = 1; i <=10; i++) {
+    // console.log(`this is for ${i}`)
+    for (let j = 1; j <=10; j++) {
+        // console.log(i + '*' +j + '='+i*j)
+    }
+    
+}
+
+
+for (let index = 0; index <=10; index++) {
+    
+    if(index==5){
+        console.log(`${index} is best number`);
+        continue;
+    }
+    console.log(index)
+}
+
+// break means stop the loop otherwise continue means just stop for ones times 
