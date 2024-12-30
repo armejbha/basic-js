3f33e4c9b2bf73117369a49b2a7f6a9519c64336
Abdur Rahim <96991837+armejbha@users.noreply.github.com>
2024-12-30 02:34:31 +0000

    truthy falsy

---
+++ 04_control/truthy.js
@@ -0,0 +1,33 @@
+const userInfo=[];
+
+if(userInfo){
+    console.log("update this profile");
+}
+else{
+    console.log("Don't touch this");
+}
+
+// which is falsy value 
+
+// "",0,-0, BigInt, null, undefined, NaN 
+
+// which is truthy value 
+
+"mejbha", "false"," ",{},[]
+
+
+// nullish coaelsing operator  ?? here return someting 
+
+let val1;
+
+val1= null?? 10;
+console.log("val1 :", val1)
+
+
+// ternary operator ?true : false
+
+let temparature =90;
+
+temparature >=100? console.log("let's them go outside"): console.log("don't let's them")
+
+
